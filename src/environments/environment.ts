77678d753e695a2249aecc9d757ffccb0120dc86
mendesdt <guilherme@dtdigitalsolutions.com>
2025-06-09 11:24:42 -0300

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   supabase: {
-    url: process.env['VITE_SUPABASE_URL'] || '',
-    anonKey: process.env['VITE_SUPABASE_ANON_KEY'] || ''
+      url: 'https://wdkjjarrmuxalvcvrryk.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indka2pqYXJybXV4YWx2Y3ZycnlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0NzgwODUsImV4cCI6MjA2NTA1NDA4NX0.WACB_AasQHtaDyrYdh1xIeva4VPYWcSYtQS5wFr_Ca8'
   }
 };
