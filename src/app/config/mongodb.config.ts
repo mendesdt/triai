06ded1f79c6d2fea28d9ded150c895530eb818f7
mendesdt <guilherme@dtdigitalsolutions.com>
2025-06-09 11:23:03 -0300

    Updated mongodb.config.ts

--- src/app/config/mongodb.config.ts
+++ src/app/config/mongodb.config.ts
@@ -1,6 +1,6 @@
 // This file is no longer needed as we're using Supabase
 // Keeping for reference but can be removed
 export const supabaseConfig = {
-  url: 'YOUR_SUPABASE_URL',
-  anonKey: 'YOUR_SUPABASE_ANON_KEY'
+  url: 'https://wdkjjarrmuxalvcvrryk.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indka2pqYXJybXV4YWx2Y3ZycnlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0NzgwODUsImV4cCI6MjA2NTA1NDA4NX0.WACB_AasQHtaDyrYdh1xIeva4VPYWcSYtQS5wFr_Ca8'
 };
